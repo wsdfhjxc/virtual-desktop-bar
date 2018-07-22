e960b953e2816d09eefa5beff46652f168e08166
wsdfhjxc <wsdfhjxc@protonmail.com>
2018-07-22 20:15:53 +0200

    Provide a way to obtain desktop info

--- plugin/mdsmodel.cpp
+++ plugin/mdsmodel.cpp
@@ -1,3 +1,16 @@
 #include "mdsmodel.h"
 
 MDSModel::MDSModel(QObject* parent) : QObject(parent) {}
+
+QVariantList MDSModel::getDesktopNames() const {
+    QVariantList desktopNames;
+    for (const QString& desktopName : desktopInfo.desktopNames()) {
+        desktopNames << QVariant(desktopName);
+    }
+    return desktopNames;
+}
+
+QVariant MDSModel::getActiveDesktopNumber() const {
+    const int currentDesktop = desktopInfo.currentDesktop();
+    return QVariant(currentDesktop);
+}
--- plugin/mdsmodel.h
+++ plugin/mdsmodel.h
@@ -2,12 +2,22 @@
 #define MDSMODEL_H
 
 #include <QObject>
+#include <QVariantList>
+#include <virtualdesktopinfo.h>
+
+using namespace TaskManager;
 
 class MDSModel : public QObject {
     Q_OBJECT
 
 public:
     MDSModel(QObject* parent = nullptr);
+
+    Q_INVOKABLE QVariantList getDesktopNames() const;
+    Q_INVOKABLE QVariant getActiveDesktopNumber() const;
+
+private:
+    VirtualDesktopInfo desktopInfo;
 };
 
 #endif // MDSMODEL_H
