d123c7e103967b7623aab3c60ca2eba69365a0e4
wsdfhjxc <wsdfhjxc@op.pl>
2020-10-31 14:40:23 +0100

    Update dependencies

--- scripts/install-dependencies-fedora.sh
+++ scripts/install-dependencies-fedora.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dnf install cmake extra-cmake-modules gcc-c++ qt5-qtbase-devel qt5-qtdeclarative-devel kf5-plasma-devel kf5-kglobalaccel-devel kf5-kxmlgui-devel
+dnf install cmake extra-cmake-modules gcc-c++ qt5-qtbase-devel qt5-qtdeclarative-devel qt5-qtx11extras-devel kf5-plasma-devel kf5-kglobalaccel-devel kf5-kxmlgui-devel
--- scripts/install-dependencies-opensuse.sh
+++ scripts/install-dependencies-opensuse.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo zypper in cmake extra-cmake-modules gcc-c++ libqt5-qtbase-devel libqt5-qtdeclarative-devel plasma-framework-devel kglobalaccel-devel kxmlgui-devel
+sudo zypper in cmake extra-cmake-modules gcc-c++ libqt5-qtbase-devel libqt5-qtdeclarative-devel libqt5-qtx11extras-devel plasma-framework-devel kglobalaccel-devel kxmlgui-devel
